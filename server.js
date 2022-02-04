db88603cb91aabc5fd47ab67a97003a722c55922
ZP <zairaperez01@gmail.com>
2022-02-04 11:13:05 -0600

    added get requests

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const path = require('path');
-
-const { animals } = require('./data/animals');
 const express = require('express');
+const { animals } = require('./data/animals');
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-
+app.use(express.static('public'));
   //parse incoming string or array data 
   app.use(express.urlencoded({ extended: true}));
   //parse incoming JSON data
@@ -93,6 +93,7 @@ app.get('/api/animals', (req,res) => {
     res.json(results);
 });
 
+
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
     if (result) {
@@ -103,9 +104,7 @@ app.get('/api/animals/:id', (req, res) => {
     
   });
 
-  app.post('/api/animals', (req, res) => {
-    // set id based on what the next index of the array will be
-    req.body.id = animals.length.toString();
+ 
   
     // add animal to json file and animals array in this function
     app.post('/api/animals', (req, res) => {
@@ -120,9 +119,26 @@ app.get('/api/animals/:id', (req, res) => {
           res.json(animal);
         }
       });
+
+
+
+
+  app.get('/', (req, res) => {
+      res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
   });
 
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
 
-app.listen(PORT, () => {
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
